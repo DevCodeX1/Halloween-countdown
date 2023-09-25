0392450b1ac6fb5cca025e64e2803d2f0e923eb2
DevCodeX <countryboya20@gmail.com>
2023-09-25 17:32:35 -0500

    added the contact page

--- script.js
+++ script.js
@@ -14,12 +14,3 @@
 
         }, 1000);
 
-
-
-function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-}
-
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
