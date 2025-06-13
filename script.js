22e89a0ce45b9c7fa38970e9841a85d53c125d8b
DevCodeX1 <countryboya20@gmail.com>
2025-06-13 16:23:51 -0500

    fixed audio and links

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var countDownDate = new Date("Oct 31, 2025 00:00:00").getTime();
-var x = setInterval(function () {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+let countDownDate = new Date("Oct 31, 2025 00:00:00").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
